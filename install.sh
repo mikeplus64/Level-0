17b4b848aeb876f4fbcf8cd2bff41fbb33f04230
Mike Ledger <eleventynine@gmail.com>
2012-04-29 09:53:36 +1000

    cabal file; no more install.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-make
-mkdir ~/.config/level_0
-touch ~/.config/level_0/score
-
-ln -s /usr/share/fonts/TTF/TerminusBold.ttf ~/.config/level_0/font.ttf || (echo "Font not found."; echo "Enter the path to the font you want to use:"; read FONT; ln -s "$FONT" ~/.config/level_0/font.ttf)
